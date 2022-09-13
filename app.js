c8ab4e0b6a534a754397ebd4c4a194c8dcba3322
CJ Pallari <56815643+cjpallari@users.noreply.github.com>
2022-09-12 23:31:23 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,42 +1,54 @@
+/* Extremely unorganized, lots of repetition, will refactor when i feel like it */
+
 let newLine = document.getElementById('flow');
 let userInput = document.querySelector('input').value;
 let asciiArt = document.querySelector('pre').textContent;
+let helpCommand = document.getElementById('helpCommand');
+let email = document.createElement('a');
+let code = 'Type <span style="color: hsl(342deg 100% 65%);">"help"</span> for a list of commands.';
 let count = 0;
+const instagram = 'https://instagram.com/cjpallari';
+const twitter = 'https://twitter.com/CJPallari';
+const linkedin = 'https://linkedin.com/in/cj-pallari-9423541b7/';
+const github = 'https://github.com/cjpallari';
 
 let help = [
-    "about\n",
-    "socials\n",
+    `about\n`,
     "projects\n",
-    "contact\n",
     "history\n",
-    "banner\n",
+    /* "banner\n", */
+    "help\n",
+    "email\n",
+    "socials - shows a list of available options\n",
+    "to access links, type 'open (name of desired link)'\n",
     "clear"
 ]
 
 let about = [
-    "My name is CJ Pallari, and I am currently studying computer science at Sacramento State University.\n"
+    "My name is CJ Pallari, and I am currently studying computer science at Sacramento State University.\n",
+    "I'm 23 years old and spend most of my time in the gym, playing/watching sports, and working on cool stuff\n",
+    "like this web terminal. If you want to reach out to me you can find my contact information by using the contact\n",
+    "command."
 ]
 
-let link = document.querySelector("social");
-link.replace(/^open/,'').replace(/open+$/,'');
-console.log(link);
-
-
 let commands = [];
+
 
 document.querySelector('.textInput').addEventListener('keypress', main);
 function main(e)
 {
     let inp = document.querySelector('.textInput').value;
     let inp1 = document.querySelector('input');
+    let socials = document.querySelector('.textInput').value.slice(5);
     if (e.key === 'Enter') { //If enter is pressed
+        console.log(socials.slice(5));
         let li = document.createElement("li");  //Allows me to create new list items
         let cmd = document.createElement("li");  //Second list item var to print command that was entered
         inp1.value = '';  //Resets text box to empty
-            count++;
-            commands.push(inp);  //Holds all commands entered for history case
-            switch (inp.toLowerCase())  //Accepted commands will be held inside
-            {
+        count++;
+        commands.push(inp);  //Holds all commands entered for history case
+        switch (inp.toLowerCase())  //Accepted commands will be held inside
+        {
                 case 'help':
                     document.querySelector('.textInput').innerHTML = '<br>';  //Adds new line after pressing enter
                     cmd.innerText = `visitor@cpcodes:~$ ${inp}`;
@@ -52,7 +64,7 @@ function main(e)
                         cmd.innerText = `visitor@cpcodes:~$ ${inp}`;
                         cmd.style.color = 'hsl(157deg 35% 41%)';
                         newLine.appendChild(cmd);
-                        li.innerText = "Some random stuff about me";
+                        li.innerText = `${about.join('')}`;
                         newLine.appendChild(li);
                         break;
                         case 'socials':
@@ -60,46 +72,94 @@ function main(e)
                             cmd.innerText = `visitor@cpcodes:~$ ${inp}`;
                             cmd.style.color = 'hsl(157deg 35% 41%)';
                             newLine.appendChild(cmd);
-                            li.innerText = `My links:\nTwitter\n Instagram\n LinkedIn`;
+                            li.innerText = `my links:\nTwitter\n Instagram\n LinkedIn\n GitHub`;
                             newLine.appendChild(li);
                             break;
-                            case `open ${link}`:
-                                if (link == 'instagram'){
-
+                            case `open ${socials}`:
+                                //inp.style.color = 'hsl(342deg 100% 65%)';
+                                if (socials == 'instagram'){
+                                    cmd.innerText = `visitor@cpcodes:~$ ${inp}`;
+                                    cmd.style.color = 'hsl(157deg 35% 41%)';
+                                    newLine.appendChild(cmd);
+                                    li.innerText = `navigating to CJ's ${socials}...`;
+                                    newLine.appendChild(li);
+                                    setTimeout('window.open(instagram)', 1000);
                                 }
-                            case 'projects':
-                                document.querySelector('.textInput').innerHTML = '<br';
-                                cmd.innerText = `visitor@cpcodes:~$ ${inp}`;
-                                cmd.style.color = 'hsl(157deg 35% 41%)';
-                                newLine.appendChild(cmd);
-                                li.innerText = "You're looking at it";
-                                newLine.appendChild(li);
+                                else if (socials == 'twitter'){
+                                    cmd.innerText = `visitor@cpcodes:~$ ${inp}`;
+                                    cmd.style.color = 'hsl(157deg 35% 41%)';
+                                    newLine.appendChild(cmd);
+                                    li.innerText = `navigating to CJ's ${socials}...`;
+                                    newLine.appendChild(li);
+                                    setTimeout('window.open(twitter)', 1000);
+                                }
+                                else if (socials == 'linkedin'){
+                                    cmd.innerText = `visitor@cpcodes:~$ ${inp}`;
+                                    cmd.style.color = 'hsl(157deg 35% 41%)';
+                                    newLine.appendChild(cmd);
+                                    li.innerText = `navigating to CJ's ${socials}...`;
+                                    newLine.appendChild(li);
+                                    setTimeout('window.open(linkedin)', 1000);
+                                }
+                                else if (socials == 'github'){
+                                    cmd.innerText = `visitor@cpcodes:~$ ${inp}`;
+                                    cmd.style.color = 'hsl(157deg 35% 41%)';
+                                    newLine.appendChild(cmd);
+                                    li.innerText = `navigating to CJ's ${socials}...`;
+                                    newLine.appendChild(li);
+                                    setTimeout('window.open(github)', 1000);
+                                }
+                                else{
+                                    cmd.innerText = `visitor@cpcodes:~$ ${inp}`;
+                                    cmd.style.color = 'hsl(157deg 35% 41%';
+                                    newLine.appendChild(cmd);
+                                    li.innerText = `${socials} was not recognized... either there was a typo or I do not currently have a ${socials} account.`;
+                                    newLine.appendChild(li);
+                                }
                                 break;
-                                case 'clear':
-                                    window.location.reload();
+                                case 'projects':
+                                    document.querySelector('.textInput').innerHTML = '<br';
+                                    cmd.innerText = `visitor@cpcodes:~$ ${inp}`;
+                                    cmd.style.color = 'hsl(157deg 35% 41%)';
+                                    newLine.appendChild(cmd);
+                                    li.innerText = "in progress... most of my projects are located on github or are private.";
+                                    newLine.appendChild(li);
                                     break;
-                                    case 'history':
-                                        document.querySelector('.textInput').innerHTML = '<br>';
-                                        cmd.innerText = `visitor@cpcodes:~$ ${inp}`;
-                                        cmd.style.color = 'hsl(157deg 35% 41%)';
-                                        newLine.appendChild(cmd);
-                                        li.innerText = `${commands}`;
-                                        newLine.appendChild(li);
+                                    case 'clear':
+                                        window.location.reload();
                                         break;
-                                        case 'banner':
-                                            document.querySelector('.textInput').innerHTML= '<br>';
+                                        case 'history':
+                                            document.querySelector('.textInput').innerHTML = '<br>';
                                             cmd.innerText = `visitor@cpcodes:~$ ${inp}`;
-                                            cmd.style.color = 'hsl(157deg 50% 41%)';
+                                            cmd.style.color = 'hsl(157deg 35% 41%)';
                                             newLine.appendChild(cmd);
-                                            li.innerText = `${asciiArt}`;
+                                            li.innerText = `${commands}`;
                                             newLine.appendChild(li);
                                             break;
-                                            default:
-                                                document.querySelector('.textInput').innerHTML = '<br>';
-                                                li.innerText = `${inp} command not found. For a list of commands, type 'help'.`;
+                                            case 'banner':
+                                                document.querySelector('.textInput').innerHTML= '<br>';
+                                                cmd.innerText = `visitor@cpcodes:~$ ${inp}`;
+                                                cmd.style.color = 'hsl(157deg 50% 41%)';
+                                                newLine.appendChild(cmd);
+                                                li.innerText = `${asciiArt}`;
                                                 newLine.appendChild(li);
                                                 break;
+                                                case 'email':
+                                                    document.querySelector('.textInput').innerHTML = '<br>';
+                                                    cmd.innerText = `visitor@cpcodes:~$ ${inp}`;
+                                                    cmd.style.color = 'hsl(157deg 50% 41%';
+                                                    newLine.appendChild(cmd);
+                                                    li.innerText = 'opening mailto:cjpallari@gmail.com...';
+                                                    newLine.appendChild(li);
+                                                    email.href='mailto:cjpallari@gmail.com';
+                                                    email.click();
+                                                    break;
+                                                    default:
+                                                        document.querySelector('.textInput').innerHTML = '<br>';
+                                                        cmd.innerText = `visitor@cpcodes:~$ ${inp}`;
+                                                        cmd.style.color = 'hsl(157deg 50% 41%';
+                                                        newLine.appendChild(cmd);
+                                                        li.innerHTML = `${inp} command not found. ${code}`;
+                                                        newLine.appendChild(li);
+                                                        break;
                                 }
-        }
-            
-};
